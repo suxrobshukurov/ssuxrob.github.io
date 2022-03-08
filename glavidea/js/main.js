50802eb23cc08fefbdf413bcfd70776229a18746
suxrobshukurov <sdead96@gmail.com>
2022-03-08 12:38:47 +0300

    added mob version

--- glavidea/js/main.js
+++ glavidea/js/main.js
@@ -1,13 +1,15 @@
-window.onload = function() {
-    let menuBtn = document.querySelector('.menu-btn');
-    let menu = document.querySelector('.header__menu');
-
-    menuBtn.addEventListener('click', function() {
-        menuBtn.classList.toggle('active');
-        menu.classList.toggle("active");
-    })
-};
-
 jQuery(function() {
+
     console.log("hello");
+
+    jQuery('#dropdown-btn').on('click', function() {
+        jQuery('.menu-submenu').toggleClass('open-submenu');
+        jQuery('.dropdown-btn').toggleClass('open');
+    });
+
+    jQuery('.menu-btn').on('click', function() {
+        jQuery('.menu-btn').toggleClass('active');
+        jQuery('.header-menu').toggleClass('active');
+
+    });
 });
