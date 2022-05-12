713c5d097f78702889e3f4f80a3497e2aedf5772
suxrobshukurov <sdead96@gmail.com>
2022-05-12 14:09:59 +0300

    added memory game

---
+++ memory-game/assets/js/main.js
@@ -0,0 +1,11 @@
+let config = {
+    type: Phaser.AUTO,
+    width: 1280,
+    height: 720,
+    rows: 2,
+    cols: 5,
+    cardGap: 5,
+    scene: new GameScene()
+}
+
+let game = new Phaser.Game(config);
