cc0e3cfbcfb90f7c78ec8b51df02e424358b932e
suxrobshukurov <sdead96@gmail.com>
2022-05-13 21:11:57 +0300

    move bg

--- shooter/assets/scripts/scenes/GameScene.js
+++ shooter/assets/scripts/scenes/GameScene.js
@@ -9,8 +9,11 @@ class GameScene extends Phaser.Scene {
     }
     update() {
         this.player.move();
+        this.bg.tilePositionX += 0.5;
     }
     createBackground() {
-        this.add.sprite(0, 0, 'bg').setOrigin(0, 0);
+        this.bg = this.add.tileSprite(0, 0, config.width, config.height, 'bg').setOrigin(0, 0);
+        console.log(this.bg);
+
     }
 }
