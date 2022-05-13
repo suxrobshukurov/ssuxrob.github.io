1a3f1b9fddbb2edec9844e60d392d7063233c8b9
suxrobshukurov <sdead96@gmail.com>
2022-05-13 17:37:17 +0300

    added gamescene.js

---
+++ shooter/assets/scripts/scenes/GameScene.js
@@ -0,0 +1,12 @@
+class GameScene extends Phaser.Scene {
+    constructor() {
+        super('Game');
+    }
+    preload() {
+        console.log('GameScene.preload');
+
+    }
+    create() {
+        console.log('GameScene.create');
+    }
+}
